48feafb46692919b094477a87ee8e7ad7515cca5
mitian233 <mitian233@yahoo.co.jp>
2024-02-12 18:30:35 +0800

    Refactor TypedJs.vue component

--- src/components/TypedJs.vue
+++ src/components/TypedJs.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import {VueTypedJs} from "vue3-typed-ts";
+import { VueTypedJs } from "vue3-typed-ts";
 import axios from "axios";
-import {onMounted, ref} from "vue";
+import { onMounted, ref } from "vue";
 
 const strings = ref<Array<string>>([]);
 
-onMounted(()=>{
-  axios.get('https://v1.hitokoto.cn/').then((res:any) => {
+onMounted(() => {
+  axios.get('https://v1.hitokoto.cn/').then((res: any) => {
     const hitokotoRes = JSON.parse(JSON.stringify(res.data));
     strings.value = [hitokotoRes.hitokoto, hitokotoRes.from];
     console.log(strings)
@@ -18,12 +18,7 @@ onMounted(()=>{
   <section class="w-full flex flex-col gap-8 mb-16 sm:mb-24">
     <div class="hero hero__bg">
       <div class="hero__content h-[40vh] flex flex-col justify-center items-center">
-        <VueTypedJs v-if="strings.length == 2"
-            :strings="strings"
-            :type-speed="50"
-            :start-delay="300"
-            :loop="true"
-            :show-cursor="true">
+        <VueTypedJs :strings="strings" :type-speed="50" :start-delay="300" :loop="true" :show-cursor="true">
           <span class="typing"></span>
         </VueTypedJs>
       </div>
