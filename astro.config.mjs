593fbd3630f48f91dd77f07bf70dcdd2f46c3d8d
neko0xff <neko_0xff@protonmail.com>
2024-05-01 11:19:39 +0800

    20240501 update1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,58 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import vue from "@astrojs/vue";
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { proseRemarkPlugin } from './prose-remark-plugin.mjs';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import remarkgraphviz from 'remark-graphviz';
+import remarkMermaid from 'astro-diagram/remark-mermaid';
+import rehypeStringify from "rehype-stringify";
+import rehypeMermaid from "rehype-mermaid";
+import rehypeGraphviz from "rehype-graphviz";
+import redotStringify from "redot-stringify";
+import robotsTxt from 'astro-robots-txt';
 
-import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.bangdream.moe/',
-  integrations: [mdx(), sitemap(), vue(), tailwind({
-    applyBaseStyles: false
-  })],
+  integrations: [
+    mdx(),
+    sitemap(),
+    vue(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    robotsTxt({
+      sitemap: [
+        'https://blog.bangdream.moe/sitemap-index.xml'
+      ]
+    }),
+  ],
   markdown: {
-    remarkPlugins: [proseRemarkPlugin, remarkMath],
-    rehypePlugins: [rehypeKatex],
+    redotPlugins: [
+      redotStringify,
+    ],
+    remarkPlugins: [
+      proseRemarkPlugin,
+      remarkMath,
+      remarkParse,
+      remarkRehype,
+      remarkgraphviz,
+      remarkMermaid,
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      //rehypeMermaid,
+      rehypeStringify,
+      rehypeGraphviz
+    ],
+    syntaxHighlight: false,
   },
+  output: 'static'
 
 });
