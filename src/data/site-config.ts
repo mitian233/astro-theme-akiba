7e6fd1f3753b57c68118e1350f83110e15763b91
mitian233 <mitian233@yahoo.co.jp>
2024-05-06 00:58:46 +0800

    feature: support customize theme toggle button style

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -66,6 +66,7 @@ export type SiteConfig = {
     postsPerPage?: number;
     projectsPerPage?: number;
     giscusConfig?: GiscusConfig;
+    darkmodeSwitcherMode: 'dot' | 'button';
 };
 
 const siteConfig: SiteConfig = {
@@ -141,6 +142,7 @@ const siteConfig: SiteConfig = {
         }
     ],
     personalizedHero: true,
+    darkmodeSwitcherMode: 'dot',
     // hero: {
     //     title: 'Hi There & Welcome to My Corner of the Web!',
     //     text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
